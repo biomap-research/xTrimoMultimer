fde7c8dbf196d4d68f3adc551f4d2f86a3b2be98
shenggan <csg19971016@gmail.com>
2022-08-15 12:48:06 +0800

    add performance benchmark for evoformer

--- xtrimomultimer/model_acc/__init__.py
+++ xtrimomultimer/model_acc/__init__.py
@@ -2,4 +2,4 @@ from .msa import MSAStack
 from .ops import OutProductMean, set_chunk_size
 from .triangle import PairStack
 
-__all__ = ["MSAStack", "OutProductMean", "PairStack", "Evoformer", "set_chunk_size"]
+__all__ = ["MSAStack", "OutProductMean", "PairStack", "set_chunk_size"]
